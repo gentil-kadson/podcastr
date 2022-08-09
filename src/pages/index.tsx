6bb57b639ab8d600e9127f389672193d8068465f
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2022-08-09 17:47:14 -0300

    treated podcast episode data; implemented api to get podcasts data; implemented utils function to convert duration to string.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
 import { GetStaticProps } from "next";
-import { Header } from "../components/Header/index";
+import { format, parseISO } from "date-fns";
+import convertDurationToTimeString from "../utils/convertDurationToTimeString";
+import { ptBR } from "date-fns/locale";
+import api from "../services/api";
 
 type Episode = {
   id: String;
   title: String;
-  members: String; 
-}
+  members: String;
+  //...
+};
 
 type HomeProps = {
   episodes: Array<Episode>;
-}
+};
 
 export default function Home(props: HomeProps) {
   return (
@@ -21,8 +25,30 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+  const { data } = await api.get(
+    "episodes?_limit=12&_sort=published_at&_order=desc",
+    {
+      params: {
+        _limit: 12,
+        _sort: "published_at",
+        _order: "desc",
+      },
+    }
+  );
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy"),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url
+    };
+  });
 
   return {
     props: {
